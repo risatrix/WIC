0d957606839ff2cfbdc0f8ea02081afd3a97cfbf
risatrix <risatrix@protonmail.com>
2025-11-06 19:48:48 -0500

    add new javascript toggle functionality

---
+++ js/app.js
@@ -0,0 +1,27 @@
+//build notes and index from existing text, then attach to toggles
+
+const notes = document.querySelectorAll('.note p');
+
+const toggles = document.querySelectorAll('.fn')
+
+notes.forEach((element, index) =>  {
+	element.setAttribute('data-index', index);
+	element.classList.add('internote');
+});
+
+
+toggles.forEach((element, index) => {
+	element.setAttribute('data-index', index);
+	var graf = element.parentElement;
+	graf.after(notes[index]);
+	element.addEventListener("click", toggleNote)
+});
+
+
+function toggleNote(e) {
+	e.preventDefault();
+	index = Number(e.target.text)-1;
+	// openNote(index);
+	notes[index].classList.toggle('active');
+	notes[index].classList.toggle('visible');
+}
